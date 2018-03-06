3c286b8408c2b8aefff76a1f34d16690c662a733
Nandor Dudas <nandor.dudas@gmail.com>
2018-03-06 21:49:59 +0100

    Model can called dynamically

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using CsharpCourse.Models;
 
 namespace CsharpCourse
@@ -7,8 +8,35 @@ namespace CsharpCourse
     {
         static void Main(string[] args)
         {
-            Task task = new Task();
-            task.Resolve();
+            try
+            {
+                string MethodName = "Resolve";
+                string TypeName = "CsharpCourse.Models.Task";
+                Type type = Type.GetType(TypeName);
+
+                if (type == null)
+                {
+                    throw new InvalidOperationException($"{TypeName} model doesn't exists");
+                }
+
+                object instance = Activator.CreateInstance(type);
+                MethodInfo method = type.GetMethod(MethodName);
+
+                if (method == null)
+                {
+                    throw new InvalidOperationException($"{MethodName} method doesn't exists");
+                }
+
+                method.Invoke(instance, null);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            // Task task = new Task();
+            // task.Resolve();
         }
     }
 }
