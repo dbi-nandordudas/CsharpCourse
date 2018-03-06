eb503c0f9a0f559095733d72f9cb981b9e43ddd7
Nandor Dudas <nandor.dudas@gmail.com>
2018-03-06 22:52:56 +0100

    Only one task added

--- Models/Lessons/Lesson_01/Task_01.cs
+++ Models/Lessons/Lesson_01/Task_01.cs
@@ -7,11 +7,35 @@ namespace CsharpCourse.Models.Lessons.Lesson_01
     {
         public Task_01()
         {
-            Description = "My first task's long description. My first task's long description. My first task's long description.";
+            Description = "Készítsen programot, amely bekér két számot és kiírja az alábbi formában a velük elvégzett műveletek eredményét.";
 
             ShowInfo();
         }
 
-        public override void Resolve() => Console.WriteLine("My first task's solution!");
+        public override void Resolve()
+        {
+            int num_01, num_02;
+
+            Console.WriteLine("Adjon meg két egész számot!");
+
+            try
+            {
+                Console.Write("Első szám: ");
+                num_01 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Második szám: ");
+                num_02 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"{num_01} + {num_02} = {num_01 + num_02}");
+                Console.WriteLine($"{num_01} * {num_02} = {num_01 * num_02}");
+                Console.WriteLine($"{num_01} - {num_02} = {num_01 - num_02}");
+                Console.WriteLine($"{num_01} / {num_02} = {num_01 / (float)num_02}");
+                Console.WriteLine($"{num_01} % {num_02} = {num_01 % num_02}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
