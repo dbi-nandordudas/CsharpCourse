c9ee33c14daca189d291bd3c65ff8d81793152c3
Nandor Dudas <nandor.dudas@gmail.com>
2018-03-06 21:57:38 +0100

    Code refactored

--- Program.cs
+++ Program.cs
@@ -12,31 +12,34 @@ namespace CsharpCourse
             {
                 string MethodName = "Resolve";
                 string TypeName = "CsharpCourse.Models.Task";
-                Type type = Type.GetType(TypeName);
 
-                if (type == null)
-                {
-                    throw new InvalidOperationException($"{TypeName} model doesn't exists");
-                }
-
-                object instance = Activator.CreateInstance(type);
-                MethodInfo method = type.GetMethod(MethodName);
-
-                if (method == null)
-                {
-                    throw new InvalidOperationException($"{MethodName} method doesn't exists");
-                }
-
-                method.Invoke(instance, null);
+                CreateAndInvoke(TypeName, MethodName);
             }
             catch (Exception e)
             {
 
                 Console.WriteLine(e.Message);
             }
+        }
 
-            // Task task = new Task();
-            // task.Resolve();
+        public static object CreateAndInvoke(string typeName, string methodName, object[] methodArgs = null)
+        {
+            Type type = Type.GetType(typeName);
+
+            if (type == null)
+            {
+                throw new InvalidOperationException($"{typeName} model doesn't exists");
+            }
+
+            object instance = Activator.CreateInstance(type);
+            MethodInfo method = type.GetMethod(methodName);
+
+            if (method == null)
+            {
+                throw new InvalidOperationException($"{methodName} method doesn't exists");
+            }
+
+            return method.Invoke(instance, methodArgs);
         }
     }
 }
